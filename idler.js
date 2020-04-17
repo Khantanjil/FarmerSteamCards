e23046b940d8ecb9c9dc5c0c63c9d16392c313b7
Khantanjil <tanjilkhan.sh@gmail.com>
2020-04-17 18:55:48 +0100

    Upload Files

--- idler.js
+++ idler.js
@@ -1,17 +1,38 @@
+const prompt = require('prompt');
 const SteamUser = require('steam-user')
-const config = require('./config.json')
+//const config = require('./config.json')
 const client = new SteamUser();
 
-const logInOptions = {
-    accountName: config.username,
-    password: config.password,
-};
+prompt.start();
+prompt.get(['username', 'password'], function(err, result){
+	if(err){return onErr(err);}
+	console.log('Your account: ');
+	console.log('	Username: ' + result.username);
+	console.log('	Password: ' + result.password);
+	console.log("Importing steam-user")
+	console.log("Trying to Logging...\n")
 
-client.logOn(logInOptions)
+	const logInOptions = {
+    	accountName: result.username,
+    	password: result.password,
+	};
 
-client.on('loggedOn', () => {
-    console.log('Logged in!')
+	client.logOn(logInOptions)
+
+	client.on('loggedOn', () => {
+    	console.log('Logged in!')
+
     client.setPersona(SteamUser.EPersonaState.Offline);
-    client.gamesPlayed([860860,303610,448070,797410]);
-    console.log('Farming...')
-});
+    // Set to offline to not disturbe te others
+    // You can change to Online if you want
+    console.log('Setting to offline status.')
+    client.gamesPlayed([504130]);
+    console.log("Playing the games and idling cards...")
+	});
+	});
+
+
+function onErr(err){
+	console.log(err);
+	return 1;
+}
